8cd85982aa2331e38985372eec5c0e67271c0ac1
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-01-30 20:17:02 +0700

    update try catch

--- try-catch-error-handling/index.js
+++ try-catch-error-handling/index.js
@@ -6,11 +6,27 @@ const p = {
     return `${this.fN} ${this.lN}`;
   },
 
-  set fullName(first) {
-    this.fN = first;
-  },
+  set fullName(v) {
+    // localhost:9999/users/1
+    // khi sử dụng split("/") và truyền pattern "/"
+    // thì nó tự động tạo array và phân cách index
+    // theo pattern truyền vào
+    // [localhost:9999, users, 1]
 
-  // phương thức set sẽ gán bất kỳ giá
-  // trị nào vào property của object
-  // mà this đại diện.
+    // NOTE: bắt lỗi thông qua if statement
+    // if (typeof v !== "string") return;
+
+    // NOTE: bắt lỗi thông qua throw error kết hợp try...catch
+    if (typeof v !== "string") throw new Error("Value is not a string.");
+
+    const parts = v.split(" ", 2);
+    this.fN = parts[0];
+    this.lN = parts[1];
+  },
 };
+
+try {
+  p.fullName = null;
+} catch (error) {
+  alert(error);
+}
